0c9dd613611975b64b8073f81c8dd030b48d0427
catter1 <63272345+catter1@users.noreply.github.com>
2020-07-27 17:58:47 -0400

    BETA 2
    -Added first Dev command

--- index.js
+++ index.js
@@ -14,4 +14,22 @@ bot.on('ready', () =>{
 
 bot.on('message', message=>{
 
+    if (message.author.bot || !message.content.startsWith(PREFIX)) return;
+    const args = message.content.slice(PREFIX.length).split(" ");
+
+    switch(args[0]){
+    case 'dev':
+        const dev = new Discord.MessageEmbed()
+        .setTitle('MTG Bot Information')
+        .setAuthor('catter1', 'https://cdn.discordapp.com/avatars/260929689126699008/a_ee5677ca4e913dc06e0408f792ddc44a.webp?size=128')
+        .setColor(0x28F5D6)
+        .setThumbnail('https://lh3.googleusercontent.com/oaLVuSSzLXtUvyh0L0itUk4LtD7zLPgSOluLJvvklfyqiTMBxwJuToXf0sDCNICuLm67aQ=s85')
+        .addField('Bot', botname)
+        .addField('Version', version)
+        .addField('Developer', devman)
+        message.channel.send(dev);
+    break;
+    }
 })
+
+bot.login(token);
